3f9b9d51d28854e2f64b65adb311731662a014c8
dipens <dipenmshah8@gmail.com>
2021-02-15 14:49:15 -0500

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -5,7 +5,7 @@ import {setContext} from 'apollo-link-context';
 
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://agile-mountain-82529.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
